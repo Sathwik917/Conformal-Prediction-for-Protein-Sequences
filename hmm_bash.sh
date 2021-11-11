4dc143afb94a3b49fc7e70c3b84a2575b7b03594
Sathwik917 <91993017+Sathwik917@users.noreply.github.com>
2021-11-11 22:18:57 +0000

    Delete hmm_bash.sh

--- hmm_bash.sh
+++
@@ -1,4 +0,0 @@
-./muscle3.8.31_i86win32.exe -in PF00069_seed.txt -out PF00069_muscle_out
-hmmbuild PF00069_hmmbuild_out.hmm PF00069_muscle_out
-hmmpress PF00069_hmmbuild_out.hmm
-hmmscan -o hmmscan_out PF00069_hmmbuild_out.hmm pf69sampleseq
