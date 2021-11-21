169742a475037b3c8f9b6b5311ad8d8beee92802
Sathwik917 <91993017+Sathwik917@users.noreply.github.com>
2021-11-21 21:14:00 +0000

    Delete project directory

--- project/hmm_bash_new.sh
+++
@@ -1,7 +0,0 @@
-rm -rf *out*
-./muscle3.8.31_i86linux64 -in PF00069_seed.txt -out PF00069_muscle_out
-hmmbuild PF00069_hmmbuild_out.hmm PF00069_muscle_out
-hmmpress PF00069_hmmbuild_out.hmm
-hmmscan --tblout PF00069_hmmscan_out PF00069_hmmbuild_out.hmm PF00069_seed.txt
-hmmsearch --tblout PF00069_hmmsearch_out PF00069_hmmbuild_out.hmm PF00069_seed.txt
-grep PF00069_muscle_out PF00069_hmmscan_out | awk '{ print $5 }' >> Evalue_out.txt
